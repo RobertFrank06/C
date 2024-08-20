092c760a45440eb2ff353f21c8376d3a4085d461
rfrank <robertfrank.work@gmail.com>
2024-08-20 16:28:08 +0200

    fix

--- Piscine/C05/ex01/ft_recursive_factorial.c
+++ Piscine/C05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: rfrank <rfrank@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 16:24:15 by rfrank            #+#    #+#             */
-/*   Updated: 2024/08/20 16:22:49 by rfrank           ###   ########.fr       */
+/*   Updated: 2024/08/20 16:27:38 by rfrank           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	ft_recursive_factorial(int nb)
 		return (1);
 	else if (nb < 0)
 		return (0);
-	return (nb * ft_recursive_factorial(nb--));
+	return (nb * ft_recursive_factorial(nb - 1));
 }
