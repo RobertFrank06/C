c4eba69192a31ea899088856b2cd646045e62920
rfrank <robertfrank.work@gmail.com>
2024-08-20 17:57:34 +0200

    fixes

--- Piscine/C05/ex03/ft_recursive_power.c
+++ Piscine/C05/ex03/ft_recursive_power.c
@@ -6,16 +6,14 @@
 /*   By: rfrank <rfrank@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 17:35:32 by rfrank            #+#    #+#             */
-/*   Updated: 2024/08/14 18:19:32 by rfrank           ###   ########.fr       */
+/*   Updated: 2024/08/20 17:57:20 by rfrank           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
 	if (power == 0)
-	{
 		return (1);
-	}
 	if (power < 0)
 		return (0);
 	return (nb *(ft_recursive_power(nb, power - 1)));
