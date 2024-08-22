59fd58a52cd06fd5976b686edba9c62dd0d1ae7c
rfrank <robertfrank.work@gmail.com>
2024-08-22 17:56:06 +0200

    fix

--- Piscine/C05/ex05/ft_sqrt.c
+++ Piscine/C05/ex05/ft_sqrt.c
@@ -6,32 +6,26 @@
 /*   By: rfrank <rfrank@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 18:20:22 by rfrank            #+#    #+#             */
-/*   Updated: 2024/08/14 19:29:59 by rfrank           ###   ########.fr       */
+/*   Updated: 2024/08/21 22:45:18 by rfrank           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb)
 {
-	int	first;
-	int	last;
-	int	guess;
-	int	sqrt;
+	int	odd;
+	int	count;
 
-	sqrt = 0;
-	first = 0;
-	last = nb;
-	if (nb <= 0)
+	if (nb < 0)
 		return (0);
-	while (first <= last)
+	odd = 1;
+	count = 0;
+	while (nb > 0)
 	{
-		guess = first + (last - first) / 2;
-		sqrt = guess * guess;
-		if (sqrt == nb)
-			return (guess);
-		else if (sqrt < nb)
-			first = guess + 1;
-		else
-			last = guess - 1;
+		nb -= odd;
+		odd += 2;
+		count++;
+		if (nb == 0)
+			return (count);
 	}
 	return (0);
 }
